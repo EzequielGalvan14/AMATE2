807635caa5d6a0788785ab8c090b41e299fd22ee
Juan Bautista Quiroga <juanbaquiroga@gmail.com>
2022-06-05 20:01:49 -0300

    creacion del modelo categorias

--- amate/Productos/admin.py
+++ amate/Productos/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from Productos.models import producto
+from Productos.models import producto,  categoria
 
 # Register your models here.
 
 admin.site.register(producto)
+admin.site.register(categoria)
--- amate/Productos/models.py
+++ amate/Productos/models.py
@@ -5,10 +5,20 @@ from django.db import models
 
 # Create your models here.
 
+class categoria(models.Model):
+    name = models.CharField(max_length=15)
+    description = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+
 class producto(models.Model):
     name = models.CharField(max_length=40)
     image = models.ImageField(upload_to='productos', null=True, blank= True)
     price = models.FloatField()
     description = models.CharField(max_length=200, blank=True, null=True)
-    category = models.CharField(max_length=30)
+    category = models.ForeignKey(categoria,  on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
+
